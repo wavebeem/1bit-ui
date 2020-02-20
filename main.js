2cac51657999e4faeabc1022e5c3fa358c534b25
Brian Mock <brian@mockbrian.com>
2020-02-20 12:52:53 -0800

    It keeps getting better

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
+function $(selector, element = document) {
+  return element.querySelector(selector);
+}
+
+function $$(selector, element = document) {
+  return Array.from(element.querySelectorAll(selector));
+}
+
 export function main() {
-  for (const element of document.querySelectorAll("[data-theme-switcher]")) {
+  setThemeColors("#222323", "#f0f6f0");
+  for (const element of $$("[data-theme-switcher]")) {
+    element.style.setProperty("--bit-color-0", element.dataset["bitTheme-0"]);
+    element.style.setProperty("--bit-color-1", element.dataset["bitTheme-1"]);
     element.addEventListener(
       "click",
       () => {
-        const { "bitTheme-0": c0, "bitTheme-1": c1 } = element.dataset;
-        element.style.setProperty("--bit-color-0", c0);
-        element.style.setProperty("--bit-color-1", c1);
-        setThemeColors(c0, c1);
+        setThemeColors(
+          element.dataset["bitTheme-0"],
+          element.dataset["bitTheme-1"]
+        );
       },
       false
     );
@@ -14,9 +25,14 @@ export function main() {
 }
 
 function setThemeColors(color0, color1) {
-  const { style } = document.documentElement;
-  style.setProperty("--bit-color-0", color0);
-  style.setProperty("--bit-color-1", color1);
+  const root = $(":root");
+  root.style.setProperty("--bit-color-0", color0);
+  root.style.setProperty("--bit-color-1", color1);
+  $("#theme-example").textContent = `\
+:root {
+  --bit-color-0: ${color0};
+  --bit-color-1: ${color1};
+}`;
 }
 
 main();
