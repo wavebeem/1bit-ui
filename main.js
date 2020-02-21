9a19e59dfc77360219986adc31419964130968c8
Brian Mock <brian@mockbrian.com>
2020-02-20 17:15:56 -0800

    get creative example

--- main.js
+++ main.js
@@ -26,12 +26,76 @@ export function main() {
     const name = element.dataset.injectExample;
     const example = examples[name];
     console.log("TODO: [example]", name, example);
+    const div = document.createElement("div");
+    div.innerHTML = example;
+    const pre = document.createElement("pre");
+    pre.className = "bit-pre";
+    pre.textContent = example;
+    element.insertAdjacentElement("beforeend", div);
+    element.insertAdjacentElement("beforeend", pre);
     // TODO: Insert as HTML
     // TODO: Insert as code snippet
   }
 }
 
-const examples = {};
+const examples = {
+  getCreative: `\
+<style>
+  .fancy-container {
+    --bit-color-0: black;
+    --bit-color-1: white;
+    border: 6px double var(--bit-color-0);
+    background: var(--bit-color-1);
+    padding: 1rem;
+  }
+
+  .theme-orange {
+    --bit-color-0: hsl(30, 100%, 20%);
+    --bit-color-1: hsl(30, 100%, 90%);
+  }
+
+  .theme-blue {
+    --bit-color-0: hsl(200, 100%, 20%);
+    --bit-color-1: hsl(200, 100%, 90%);
+  }
+
+  .theme-green {
+    --bit-color-0: hsl(120, 100%, 20%);
+    --bit-color-1: hsl(120, 100%, 90%);
+  }
+
+  .theme-red {
+    --bit-color-0: hsl(0, 100%, 20%);
+    --bit-color-1: hsl(0, 100%, 90%);
+  }
+</style>
+<div class="site-columns-auto fancy-container">
+  <button class="bit-button theme-blue">
+    Blue
+  </button>
+  <button class="bit-button theme-green">
+    Green
+  </button>
+  <button class="bit-button theme-red">
+    Red
+  </button>
+  <button class="bit-button theme-orange">
+    Orange
+  </button>
+  <button
+    class="bit-button"
+    style="
+      --bit-color-0: #222222;
+      --bit-color-1: #eeeeee;
+    "
+  >
+    Inline Styles
+  </button>
+  <button class="bit-button">
+    Default
+  </button>
+</div>`
+};
 
 function setThemeColors(color0, color1) {
   const root = $(":root");
