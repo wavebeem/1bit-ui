072bd4140d43d92ec4eacdbb9f4ae42822a8ca9a
Brian Mock <brian@mockbrian.com>
2020-03-02 12:49:42 -0800

    use shelljs for the build

--- bin/build.js
+++ bin/build.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const fs = require("fs");
+const sh = require("shelljs");
 const path = require("path");
 
 const { version } = require("../package.json");
@@ -13,14 +13,13 @@ const header = `\
  * 1bit-ui v${version}
  * Copyright ${year} Brian Mock
  */
+
 `;
 
-const srcFile = path.join(__dirname, "../src/1bit-ui.css");
-const srcCSS = fs.readFileSync(srcFile, "utf-8");
+const srcCSS = sh.cat(path.join(__dirname, "../src/1bit-ui.css")).stdout;
 const distCSS = header + srcCSS;
 const distDir = path.join(__dirname, "../dist");
 const distFile = path.join(distDir, "1bit-ui.css");
-if (!fs.existsSync(distDir)) {
-  fs.mkdirSync(distDir);
-}
-fs.writeFileSync(distFile, distCSS, "utf-8");
+sh.rm("-rf", distDir);
+sh.mkdir("-p", distDir)
+sh.ShellString(distCSS).to(distFile);
