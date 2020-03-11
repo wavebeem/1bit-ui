c17e8e965505c7658298f4f891a6627f7fe9e8eb
Brian Mock <brian@mockbrian.com>
2020-03-10 21:40:15 -0700

    more custom elements

--- src/docs/docs.js
+++ src/docs/docs.js
@@ -6,22 +6,6 @@ function $(selector, element = document) {
   return element.querySelector(selector);
 }
 
-class TableOfContentsElement extends HTMLElement {
-  connectedCallback() {
-    this.classList.add("bit-card", "site-toc");
-    for (const h2 of $$("h2")) {
-      const a = document.createElement("a");
-      a.href = `#${h2.id}`;
-      a.textContent = h2.textContent;
-      a.className = "bit-link";
-      this.appendChild(a);
-      h2.innerHTML = `<a href="#${h2.id}" class="site-link-header">${h2.innerHTML}</a>`;
-    }
-  }
-}
-
-customElements.define("table-of-contents", TableOfContentsElement);
-
 function htmlToCode(code) {
   const lines = code
     .replace(/^\n*/, "")
@@ -56,50 +40,10 @@ for (const value of Object.values(bitRootStyle)) {
   }
 }
 
-function getCustomProperties(element) {
-  return (
-    element.dataset.injectExampleProperties ||
-    element.dataset.customProperties ||
-    element.getAttribute("properties") ||
-    ""
-  )
-    .trim()
-    .split(/\s+/)
-    .filter(x => x);
-}
-
-function initializeCustomPropertyEditor(properties, propertyEditor) {
-  propertyEditor.className = "bit-card site-property-editor";
-  const title = document.createElement("h3");
-  title.className = "site-property-editor-title";
-  title.textContent = "CSS Custom Properties";
-  propertyEditor.appendChild(title);
-  const grid = document.createElement("div");
-  grid.className = "site-property-editor-grid";
-  propertyEditor.appendChild(grid);
-  for (const prop of properties) {
-    const label = document.createElement("label");
-    label.textContent = prop;
-    const input = document.createElement("input");
-    input.className = "bit-input";
-    input.placeholder = baseCustomProperties[prop];
-    input.addEventListener(
-      "input",
-      event => {
-        bitRoot.style.setProperty(prop, event.target.value);
-      },
-      false
-    );
-    grid.appendChild(label);
-    grid.appendChild(input);
-  }
-  return propertyEditor;
-}
-
 class InjectExampleElement extends HTMLElement {
   connectedCallback() {
     this.classList.add("site-example");
-    const name = this.dataset.injectExample || this.getAttribute("example");
+    const name = this.dataset.example;
     const template = document.getElementById(`template-${name}`);
     const div = document.createElement("div");
     div.dataset.exampleName = name;
@@ -114,10 +58,9 @@ class InjectExampleElement extends HTMLElement {
     pre.dataset.exampleType = "html";
     const preH3 = document.createElement("h3");
     preH3.textContent = "Code";
-    const properties = getCustomProperties(this);
-    if (properties.length > 0) {
-      const propertyEditor = document.createElement("div");
-      initializeCustomPropertyEditor(properties, propertyEditor);
+    if ("properties" in this.dataset) {
+      const propertyEditor = document.createElement("custom-properties-editor");
+      propertyEditor.dataset.properties = this.dataset.properties;
       this.insertAdjacentElement("beforeend", propertyEditor);
     }
     this.insertAdjacentElement("beforeend", divH3);
@@ -131,7 +74,34 @@ customElements.define("inject-example", InjectExampleElement);
 
 class CustomPropertiesEditorElement extends HTMLElement {
   connectedCallback() {
-    initializeCustomPropertyEditor(getCustomProperties(this), this);
+    const properties = (this.dataset.properties || "")
+      .trim()
+      .split(/\s+/)
+      .filter(x => x);
+    this.classList.add("bit-card", "site-property-editor");
+    const title = document.createElement("h3");
+    title.className = "site-property-editor-title";
+    title.textContent = "CSS Custom Properties";
+    this.appendChild(title);
+    const grid = document.createElement("div");
+    grid.className = "site-property-editor-grid";
+    this.appendChild(grid);
+    for (const prop of properties) {
+      const label = document.createElement("label");
+      label.textContent = prop;
+      const input = document.createElement("input");
+      input.className = "bit-input";
+      input.placeholder = baseCustomProperties[prop];
+      input.addEventListener(
+        "input",
+        event => {
+          bitRoot.style.setProperty(prop, event.target.value);
+        },
+        false
+      );
+      grid.appendChild(label);
+      grid.appendChild(input);
+    }
   }
 }
 
@@ -139,3 +109,19 @@ customElements.define(
   "custom-properties-editor",
   CustomPropertiesEditorElement
 );
+
+class TableOfContentsElement extends HTMLElement {
+  connectedCallback() {
+    this.classList.add("bit-card", "site-toc");
+    for (const h2 of $$("h2")) {
+      const a = document.createElement("a");
+      a.href = `#${h2.id}`;
+      a.textContent = h2.textContent;
+      a.className = "bit-link";
+      this.appendChild(a);
+      h2.innerHTML = `<a href="#${h2.id}" class="site-link-header">${h2.innerHTML}</a>`;
+    }
+  }
+}
+
+customElements.define("table-of-contents", TableOfContentsElement);
